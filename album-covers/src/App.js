3151bf5820b26f494bf070746e573ab050d2ee65
Shani <shani.getz@gmail.com>
2022-05-22 18:40:20 +0300

    fix

--- album-covers/src/App.js
+++ album-covers/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Gallery from 'react-grid-gallery';
 
@@ -9,20 +8,6 @@ function App() {
   return (
     <div className="App">
       <Gallery images={images}/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
