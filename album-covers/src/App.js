c0aa689f379db48147c0993eeea8760c03558a30
Shani <shani.getz@gmail.com>
2022-05-26 14:56:20 +0300

    fix

--- album-covers/src/App.js
+++ album-covers/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Gallery from 'react-grid-gallery';
 
-const data = require("./data/albums_test.json");
-const images = data.albums;
+const data = require("./data/albums_test.json").albums;
 
 function App() {
   return (
     <div className="App">
-      <Gallery images={images}/>
+      <Gallery images={data}/>
     </div>
   );
 }
